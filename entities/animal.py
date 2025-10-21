f81ee0706304ef20f93f34fe9c82b63c3b52c528
Miguel <gosvaldo372@gmail.com>
2025-10-21 11:27:46 -0700

    datos de la tabla

--- entities/animal.py
+++ entities/animal.py
@@ -8,5 +8,16 @@ class Animal:
             cls("Jirafa", "Naranja"),
             cls("Cebra", "blanco"),
             cls("Xenomorfo", "negro"),
+            cls("Elefante", "gris"),
+            cls("Tigre", "naranja con negro"),
+            cls("Panda", "blanco con negro"),
+            cls("León", "amarillo"),
+            cls("Serpiente", "verde"),
+            cls("Cocodrilo", "verde oscuro"),
+            cls("Pingüino", "blanco y azul"),
+            cls("Delfín", "azul claro"),
+            cls("Tortuga", "marrón"),
+            cls("Camaleón", "multicolor"),
+            cls("Lobo", "gris oscuro")
         ]
         return animals
